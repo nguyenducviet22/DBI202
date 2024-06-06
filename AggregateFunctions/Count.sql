e7b163b2fb5c8eed0b3b65fef4270ceafd177d70
nguyenducviet22 <ndv6060@gmail.com>
2024-06-07 06:59:05 +0700

    update 4/6

--- AggregateFunctions/Count.sql
+++ AggregateFunctions/Count.sql
@@ -3,6 +3,8 @@ USE Northwind
 -- SELECT ColName, HamGomNhom(), ...from <Table>
 -- SELECT ColName, HamGomNhom(), ...from <Table> WHERE... GROUP BY (GomTheoCumColumnNao)
 -- SELECT ColName, HamGomNhom(), ...from <Table> WHERE... GROUP BY (GomTheoCumColumnNao) HAVING
+-- COUNT(*): count rows
+-- COUNT(nameColumn): count column with value (or without NULL)
 
 SELECT * from Employees
 SELECT COUNT(*) AS [Number of employees] from Employees
@@ -21,3 +23,9 @@ SELECT City, COUNT(City) AS [No employees] from Employees GROUP BY City
 SELECT City, COUNT(City) AS [No employees] from Employees GROUP BY City HAVING COUNT(City) >= 2
 
 SELECT City, COUNT(City) from Employees WHERE City IN('London', 'Seattle') GROUP BY City
+
+SELECT * from Orders WHERE ShipCountry = 'USA'
+SELECT COUNT(ShipCountry) AS [No Orders from USA] from Orders WHERE ShipCountry = 'USA'
+SELECT ShipCountry, COUNT(*) AS [No Orders] from Orders WHERE ShipCountry IN('France', 'UK', 'USA') GROUP BY ShipCountry
+SELECT ShipCountry, COUNT(*) from Orders WHERE ShipCountry IN('France', 'UK', 'USA') 
+										GROUP BY ShipCountry HAVING COUNT (ShipCountry) >= 100
