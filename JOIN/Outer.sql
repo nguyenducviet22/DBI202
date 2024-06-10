c501d6748d3dcd05baf834755735252239d327eb
nguyenducviet22 <ndv6060@gmail.com>
2024-06-10 07:17:33 +0700

    update 10/6

---
+++ JOIN/Outer.sql
@@ -0,0 +1,11 @@
+﻿USE Cartesian
+
+INSERT INTO EnDict VALUES(4, 'Four')
+INSERT INTO ChDict VALUES(5, N'五')
+SELECT * FROM EnDict
+SELECT * FROM ChDict
+DELETE FROM ChDict WHERE Num = 5;
+
+SELECT * FROM EnDict e LEFT JOIN ChDict c ON e.Num = c.Num
+SELECT * FROM EnDict e RIGHT JOIN ChDict c ON e.Num = c.Num
+SELECT * FROM EnDict e FULL JOIN ChDict c ON e.Num = c.Num
