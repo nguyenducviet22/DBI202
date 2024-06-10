e0ff2220c63b595e02df011ab8209697fa3d1c86
nguyenducviet22 <ndv6060@gmail.com>
2024-06-11 05:59:03 +0700

    update 11/6

--- JOIN/Outer.sql
+++ JOIN/Outer.sql
@@ -4,8 +4,11 @@ INSERT INTO EnDict VALUES(4, 'Four')
 INSERT INTO ChDict VALUES(5, N'五')
 SELECT * FROM EnDict
 SELECT * FROM ChDict
+DELETE FROM EnDict WHERE Num = 4;
 DELETE FROM ChDict WHERE Num = 5;
 
 SELECT * FROM EnDict e LEFT JOIN ChDict c ON e.Num = c.Num
 SELECT * FROM EnDict e RIGHT JOIN ChDict c ON e.Num = c.Num
 SELECT * FROM EnDict e FULL JOIN ChDict c ON e.Num = c.Num
+
+SELECT * FROM EnDict e LEFT JOIN ChDict c ON e.Num = c.Num WHERE e.Num >= 3
