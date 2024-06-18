68cc1287c8229eacc0b9c62adc17b9fae741f27f
nguyenducviet22 <ndv6060@gmail.com>
2024-06-19 05:11:59 +0700

    update 18/6

---
+++ Database/01-One Table.sql
@@ -0,0 +1,36 @@
+﻿CREATE DATABASE DBDESIGN_ONETABLE
+USE DBDESIGN_ONETABLE
+
+--DROP TABLE Student
+CREATE TABLE StudentV1
+(
+	StudentID char(8),
+	LastName nvarchar(40),
+	FirstName nvarchar(10),
+	DOB datetime,
+	[Address] nvarchar(50)
+)
+
+SELECT * FROM StudentV1
+INSERT INTO StudentV1 VALUES
+('SE123456', N'Nguyễn ', N'An', '2003-01-01', N'Thủ Đức-Hồ Chí Minh')
+INSERT INTO StudentV1 VALUES
+('SE200001', N'Lê', N'Bình', '2003-01-02', NULL)
+INSERT INTO StudentV1(StudentID, LastName, FirstName, Address)
+VALUES('SE200002', N'Võ', N'Cường', 'TDM - Bình Dương')
+
+CREATE TABLE StudentV2
+(
+	StudentID char(8) PRIMARY KEY,
+	LastName nvarchar(40) NOT NULL,
+	FirstName nvarchar(10) NOT NULL,
+	DOB datetime,
+	[Address] nvarchar(50)
+)
+
+INSERT INTO StudentV2 VALUES
+('SE123456', N'Nguyễn', N'An', '2003-01-01', N'Thủ Đức-Hồ Chí Minh')
+INSERT INTO StudentV2 VALUES
+('SE200001', N'Lê', N'Bình', '2003-01-02', NULL)
+INSERT INTO StudentV2(StudentID, LastName, FirstName, Address)
+VALUES('SE200002', N'Võ', N'Cường', 'TDM - Bình Dương')
