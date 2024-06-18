48b9ea4faf5f3ada3eaa70aeb6d07f83b596587d
nguyenducviet22 <ndv6060@gmail.com>
2024-06-19 06:14:41 +0700

    update 19/6

--- Database/01-One Table.sql
+++ Database/01-One Table.sql
@@ -34,3 +34,16 @@ INSERT INTO StudentV2 VALUES
 ('SE200001', N'Lê', N'Bình', '2003-01-02', NULL)
 INSERT INTO StudentV2(StudentID, LastName, FirstName, Address)
 VALUES('SE200002', N'Võ', N'Cường', 'TDM - Bình Dương')
+
+CREATE TABLE StudentV3
+(
+	StudentID char(8),
+	LastName nvarchar(40) NOT NULL,
+	FirstName nvarchar(10) NOT NULL,
+	DOB datetime,
+	[Address] nvarchar(50),
+	CONSTRAINT PK_STUDENTV6 PRIMARY KEY(StudentID)
+)
+
+ALTER TABLE StudentV3 DROP CONSTRAINT PK_STUDENTV6
+ALTER TABLE StudentV3 ADD CONSTRAINT PK_STUDENTV3 PRIMARY KEY(StudentID)
